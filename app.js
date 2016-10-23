d0d6a6dbbf078fee61623fb63d492f43ca023ec2
mikat <mikaturk@me.com>
2016-10-23 17:27:14 +0200

    v0.0.1 release

---
+++ app.js
@@ -0,0 +1,23 @@
+var Discord = require('discord.io');
+var bot = new Discord.Client({
+    token: process.env.token,
+    autorun: true
+});
+ 
+bot.on('ready', function() {
+    console.log(bot.username + " - (" + bot.id + ")");
+});
+ 
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message === "hi bot") {
+        bot.sendMessage({
+            to: channelID,
+            message: `hey @${user}#${userID}, nice to meet you.`
+        });
+        
+        bot.sendMessage({
+          to: channelID,
+          message: `info: user: ${user}, userID: ${userID}, channelID: ${channelID}, message: ${message}, event: ${event}.`
+        })
+    }
+});
