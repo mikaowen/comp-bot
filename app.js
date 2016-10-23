22dfac5103a2a47eaeef5179cd4148b56f71d7d4
mikat <mikaturk@me.com>
2016-10-23 18:23:18 +0200

    v0.0.2

--- app.js
+++ app.js
@@ -7,17 +7,23 @@ var bot = new Discord.Client({
 bot.on('ready', function() {
     console.log(bot.username + " - (" + bot.id + ")");
 });
- 
+
 bot.on('message', function(user, userID, channelID, message, event) {
-    if (message === "hi bot") {
+    switch (message) {
+      case "hi bot":
         bot.sendMessage({
             to: channelID,
             message: `hey <@${userID}>, nice to meet you.`
         });
-        
+      case "yeet":
         bot.sendMessage({
-          to: channelID,
-          message: `info: user: ${user}, userID: ${userID}, channelID: ${channelID}, message: ${message}, event: ${event}.`
-        })
+            to: channelID,
+            message: `skrr`
+        });
     }
 });
+
+// bot.sendMessage({
+//   to: channelID,
+//   message: `info: user: ${user}, userID: ${userID}, channelID: ${channelID}, message: ${message}, event: ${event}.`
+// })
